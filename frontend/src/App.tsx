df13581aa099c8f5a5b99dd209ebdc5e301fa211
NormalMood <littlebigdiamond@gmail.com>
2024-02-24 21:11:10 +0300

    feat: added navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import Header from './components/layout/Header/Header';
 import AppRouter from './components/pages/AppRouter'
+import Navbar from './components/ui/Navbar/Navbar';
 
 function App() {
   return (
     <>
       <Header>
-        <h3>Header</h3>
+        <h1>Load Tester</h1>
+        <Navbar />
       </Header>
       <AppRouter />
     </>
